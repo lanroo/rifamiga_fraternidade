41807cfc1b9b5a3e02c049ad46fe2abb1b60c119
lanroo <yladacs@gmail.com>
2023-11-29 21:27:58 -0300

    login refresh

---
+++ src/components/FloattingNumbers.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
+const FloatingNumbers = () => {
+  const generateRandomNumber = () => {
+    return {
+      id: Math.random(),
+      number: Math.floor(Math.random() * 10),
+      initialX: Math.random() * window.innerWidth,
+      initialY: Math.random() * window.innerHeight,
+      animateX: Math.random() * window.innerWidth,
+      animateY: Math.random() * window.innerHeight,
+      delay: Math.random() * 5,
+    };
+  };
+
+  const numbers = Array.from({ length: 30 }, generateRandomNumber);
+
+  return (
+    <AnimatePresence>
+      {numbers.map((item) => (
+        <motion.span
+          key={item.id}
+          initial={{ x: item.initialX, y: item.initialY, opacity: 0 }}
+          animate={{
+            x: item.animateX,
+            y: item.animateY,
+            opacity: [0, 1, 1, 0],
+            scale: [0, 1, 1, 0]
+          }}
+          exit={{ opacity: 0 }}
+          transition={{
+            duration: 5,
+            delay: item.delay,
+            repeat: Infinity,
+            repeatType: 'loop',
+            ease: 'easeInOut'
+          }}
+          style={{
+            position: 'fixed',
+            fontSize: '20px',
+            color: 'rgba(255, 255, 255, 0.7)',
+            userSelect: 'none',
+            pointerEvents: 'none',
+          }}
+        >
+          {item.number}
+        </motion.span>
+      ))}
+    </AnimatePresence>
+  );
+};
+
+export default FloatingNumbers;
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 import './styles/LoginPage.css';
+import FloatingNumbers from '../components/FloattingNumbers'; 
 
 const LoginPage: React.FC = () => {
   const [email, setEmail] = useState('');
@@ -7,23 +9,36 @@ const LoginPage: React.FC = () => {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    // testando
     console.log(`Login com email: ${email} e senha: ${senha}`);
   };
 
   return (
-    <div className="login-container">
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="input-container">
-          <input type="email" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} />
-        </div>
-        <div className="input-container">
-          <input type="password" placeholder="Senha" value={senha} onChange={(e) => setSenha(e.target.value)} />
-        </div>
-        <button type="submit" className="button-login">Entrar</button>
-      </form>
-    </div>
+    <>
+      <FloatingNumbers /> 
+      <motion.div
+        className="login-container"
+        initial={{ opacity: 0, y: 20 }} 
+        animate={{ opacity: 1, y: 0 }} 
+        transition={{ duration: 0.5 }} 
+      >
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-container">
+            <input type="email" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div className="input-container">
+            <input type="password" placeholder="Senha" value={senha} onChange={(e) => setSenha(e.target.value)} />
+          </div>
+          <motion.button
+            type="submit"
+            className="button-login"
+            whileHover={{ scale: 1.05 }} 
+          >
+            Entrar
+          </motion.button>
+        </form>
+      </motion.div>
+    </>
   );
 };
 
