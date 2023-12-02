615fc8b28bb90ed13abc8fd0b3d0a89a8618a1ae
lanroo <yladacs@gmail.com>
2023-12-02 16:06:43 -0300

    contact page style

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 
 const ContactPage: React.FC = () => {
+  // Form submission handler
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    // O processamento do formulário seria aqui
+  };
+
   return (
-    <div>
+    <div className="contact-container">
       <h1>Contato</h1>
       <p>Tem alguma pergunta ou precisa entrar em contato conosco? Envie-nos uma mensagem!</p>
-      <form>
-        <input type="text" placeholder="Seu nome" />
-        <input type="email" placeholder="Seu e-mail" />
-        <textarea placeholder="Sua mensagem"></textarea>
+      <form onSubmit={handleSubmit} action="mailto:yladacz@gmail.com" method="GET">
+        <div className="input-group">
+          <input type="text" name="name" placeholder="Seu nome" required />
+        </div>
+        <div className="input-group">
+          <input type="email" name="email" placeholder="Seu e-mail" required />
+        </div>
+        <div className="input-group">
+          <textarea name="message" placeholder="Sua mensagem" required></textarea>
+        </div>
         <button type="submit">Enviar Mensagem</button>
       </form>
-      
     </div>
   );
 };
