d24d4657aae3eea41324f7db4123dc6efb54c549
lanroo <yladacs@gmail.com>
2024-06-30 21:25:34 -0300

    about page fix

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -6,14 +6,14 @@ const AboutPage: React.FC = () => {
     <div className="container">
       <h1 className="heading">Sobre o Rifaqui</h1>
       <p className="paragraph">
-        Bem-vindo ao Rifaqui, a plataforma de rifas online mais moderna e simples de usar. 
-        Estamos aqui para facilitar a criação e participação em rifas e sorteios de forma rápida e prática.
-        Com o Rifaqui, você pode criar sua própria rifa em apenas alguns minutos e começar a divulgá-la imediatamente.
-        Experimente a facilidade de ter sua rifa ativa e pronta para receber participantes. 
-        Cadastre seu Pix, compartilhe com seus amigos e receba os pagamentos diretamente na sua conta.
+        Bem-vindo à nossa plataforma de Bingo Fraterno, criada com o objetivo de apoiar o centro espírita na realização de bingos beneficentes. Nossa missão é arrecadar fundos para ajudar a manter e expandir as atividades e projetos da casa espírita, contribuindo para o bem-estar e a caridade na comunidade.
+        <br /><br />
+        Como uma organização sem fins lucrativos, estamos comprometidos em proporcionar um ambiente seguro e divertido para todos os participantes. Cada evento de bingo é uma oportunidade de união, solidariedade e alegria, onde todos os recursos arrecadados são destinados a causas nobres e essenciais.
+        <br /><br />
+        Participe dos nossos bingos fraternos e ajude-nos a fazer a diferença!
       </p>
     </div>
   );
 };
-
+  
 export default AboutPage;
